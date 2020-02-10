adb2a804d197821baf231630feaa635128cb0a04
Zula <zinowiewa@yandex.ru>
2020-02-10 22:20:09 +0700

    add test

--- tests/test_gendiff_plain.py
+++ tests/test_gendiff_plain.py
@@ -7,5 +7,10 @@ file = open('./tests/fixtures/result.txt', 'r')
 answer = file.read()
 
 
+file2 = open('./tests/fixtures/result2.txt', 'r')
+answer2 = file2.read()
+
 def test_answer():
     assert answer == generate_diff.engine('./tests/fixtures/before.json', './tests/fixtures/after.json')
+    assert answer2 == generate_diff.engine('./tests/fixtures/before_empty.json', './tests/fixtures/after_empty.json')
+
