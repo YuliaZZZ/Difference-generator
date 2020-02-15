cfacb1cf841b3d6a7268c4cdf8caf8ce626406c4
Zula <zinowiewa@yandex.ru>
2020-02-15 20:23:17 +0700

    new decision optima

--- gen_diff/generate_diff.py
+++ gen_diff/generate_diff.py
@@ -1,33 +1,38 @@
 from gen_diff.parsers import parser
 
 
-def string(g):
+def diff_to_string(f):
     diff = ''
-    for i in g:
-        diff += ' {}: {}\n'.format(i, g[i])
+    for i in f:
+        diff += ' {}: {}\n'.format(i, f[i])
     diff = diff.join(['{\n', '}\n'])
     return diff
 
 
-def difs(f1, f2):
+def difs(f, k, v):
     diff = {}
-    for k, v in f1.items():
-        if k in f2:
-            if v == f2[k]:
-                diff["  " + k] = v
-            else:
-                diff['- ' + k] = v
-                diff['+ ' + k] = f2[k]
+    if k in f:
+        if v == f[k]:
+            diff["  " + k] = v
         else:
             diff['- ' + k] = v
+            diff['+ ' + k] = f[k]
+    else:
+        diff['- ' + k] = v
+    return diff
+
+
+def differ(f1, f2):
+    diff = {}
+    for k, v in f1.items():
+        diff.update(difs(f2, k, v))
     for k1, v1 in f2.items():
         if k1 not in f1:
-            diff['+ ' + k1] = v1
+            diff.update({'+ ' + k1: v1})
     return diff
 
 
 def engine(file1, file2):
     f1 = parser(file1)
     f2 = parser(file2)
-    diff = difs(f1, f2)
-    return string(diff)
+    return diff_to_string(differ(f1, f2))
