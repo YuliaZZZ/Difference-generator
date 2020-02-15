ce3a8575c1e14d239feeb3f933328b9a13969b34
Zula <zinowiewa@yandex.ru>
2020-02-15 19:33:38 +0700

    new decision

--- gen_diff/generate_diff.py
+++ gen_diff/generate_diff.py
@@ -1,35 +1,33 @@
 from gen_diff.parsers import parser
 
 
-STATUS = {'deleted': '- ', 'added': '+ '}
+def string(g):
+    diff = ''
+    for i in g:
+        diff += ' {}: {}\n'.format(i, g[i])
+    diff = diff.join(['{\n', '}\n'])
+    return diff
 
 
-def string(a, b, z='  '):
-    return '{} {}: {}\n'.format(z, a, b)
-
-
-def difs(d, k, v):
-    if k in d:
-        if d[k] == v:
-            return string(k, v)
+def difs(f1, f2):
+    diff = {}
+    for k, v in f1.items():
+        if k in f2:
+            if v == f2[k]:
+                diff["  " + k] = v
+            else:
+                diff['- ' + k] = v
+                diff['+ ' + k] = f2[k]
         else:
-            st = string(k, v, STATUS['deleted'])
-            st = st + string(k, d[k], STATUS['added'])
-            return st
-    else:
-        return string(k, v, STATUS['deleted'])
+            diff['- ' + k] = v
+    for k1, v1 in f2.items():
+        if k1 not in f1:
+            diff['+ ' + k1] = v1
+    return diff
 
 
 def engine(file1, file2):
     f1 = parser(file1)
     f2 = parser(file2)
-    diff = ''
-    for k, v in f1.items():
-        diff += difs(f2, k, v)
-    for k1, v1 in f2.items():
-        if k1 not in f1:
-            diff += string(k1, v1, STATUS['added'])
-    if len(diff) == 0:
-        diff = "There's no difference.\n"
-    diff = diff.join(['{\n', '}\n'])
-    return diff
+    diff = difs(f1, f2)
+    return string(diff)
